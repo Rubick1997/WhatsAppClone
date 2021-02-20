7b39a0752f049b0f9227d8f079503703cbac9ecd
Rustam <rustam905@gmail.com>
2021-02-20 15:15:00 -0800

    connect User from cognito to the database

--- App.tsx
+++ App.tsx
@@ -1,18 +1,57 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import Amplify from "aws-amplify";
+import Amplify, { Auth, API, graphqlOperation } from "aws-amplify";
 import Config from "./src/aws-exports";
-import { withAuthenticator } from 'aws-amplify-react-native';
-
+import { withAuthenticator } from "aws-amplify-react-native";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 Amplify.configure(Config);
 
- function App() {
+const randomImages = "https://picsum.photos/200/300?random=2";
+
+function App() {
 	const isLoadingComplete = useCachedResources();
 	const colorScheme = useColorScheme();
+	//runs only when app is first mounted
+
+	useEffect( () => {
+		const fetchUser = async () => {
+		  const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true });
+	
+		  if (userInfo) {
+			const userData = await API.graphql(
+			  graphqlOperation(
+				getUser,
+				{ id: userInfo.attributes.sub }
+				)
+			)
+	
+			if (userData.data.getUser) {
+			  console.log("User is already registered in database");
+			  return;
+			}
+	
+			const newUser = {
+			  id: userInfo.attributes.sub,
+			  name: userInfo.username,
+			  imageUri: randomImages,
+			  status: 'Hey, I am using WhatsApp',
+			}
+	
+			await API.graphql(
+			  graphqlOperation(
+				createUser,
+				{ input: newUser }
+			  )
+			)
+		  }
+		}
+		fetchUser();
+	  }, [])
 
 	if (!isLoadingComplete) {
 		return null;
@@ -25,5 +64,4 @@ Amplify.configure(Config);
 		);
 	}
 }
-export default withAuthenticator(App)
-
+export default withAuthenticator(App);
