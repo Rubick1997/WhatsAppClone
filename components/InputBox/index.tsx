d7154d121ef9eeec21731888b4f007c8f6881c8d
Rustam <rustam905@gmail.com>
2021-02-19 19:14:11 -0800

    Input was made

---
+++ components/InputBox/index.tsx
@@ -0,0 +1,67 @@
+import {
+	MaterialCommunityIcons,
+	FontAwesome5,
+	Entypo,
+	Fontisto,
+	MaterialIcons,
+} from "@expo/vector-icons";
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import styles from "./styles";
+
+const InputBox = () => {
+	const [message, setMessage] = useState("");
+
+	const onMicrophonePress = () => {
+		console.warn("Microphone");
+	};
+
+	const onSendPress = () => {
+		console.warn("Message Sent");
+		setMessage("");
+	};
+
+	const onPress = () => {
+		if (!message) {
+			onMicrophonePress();
+		} else {
+			onSendPress();
+		}
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.mainContainer}>
+				<FontAwesome5
+					name='laugh-beam'
+					size={24}
+					color='grey'
+					position='fixed'
+				/>
+				<TextInput
+					placeholder='Type a message'
+					style={styles.textInput}
+					multiline
+					value={message}
+					onChangeText={setMessage}
+				/>
+				<Entypo name='attachment' size={24} color='grey' style={styles.icon} />
+				{!message && (
+					<Fontisto name='camera' size={24} color='grey' style={styles.icon} />
+				)}
+			</View>
+			<TouchableOpacity onPress={onPress}>
+				<View style={styles.buttonContainer}>
+					{!message ? (
+						<MaterialCommunityIcons name='microphone' size={28} color='white' />
+					) : (
+						<MaterialIcons name='send' size={28} color='white' />
+					)}
+				</View>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+export default InputBox;
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -5,20 +5,21 @@ import { FlatList } from "react-native-gesture-handler";
 import chatRoomData from "../data/Chats";
 import ChatMessage from "../components/ChatMessage";
 import BG from "../assets/images/BG.png";
+import InputBox from "../components/InputBox";
 
 const ChatRoomScreen = () => {
 	const route = useRoute();
 
 	// console.log(route.params)
 	return (
-		<ImageBackground style={{width:"100%",height:"100%"}} source={BG}>
-			<View>
-				<FlatList
-					data={chatRoomData.messages}
-					renderItem={({ item }) => <ChatMessage message={item} />}
-					inverted
-				/>
-			</View>
+		<ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
+			<FlatList
+				data={chatRoomData.messages}
+				renderItem={({ item }) => <ChatMessage message={item} />}
+				inverted
+			/>
+
+			<InputBox />
 		</ImageBackground>
 	);
 };
