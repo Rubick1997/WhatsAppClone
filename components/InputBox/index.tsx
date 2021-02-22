e60431c73fe1fbe80a37bdbeb239657bca9ae35b
Rustam <rustam905@gmail.com>
2021-02-21 20:25:20 -0800

    Keyboard works fine

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -6,7 +6,7 @@ import {
 	MaterialIcons,
 } from "@expo/vector-icons";
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, KeyboardAvoidingView } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import styles from "./styles";
 import { API, graphqlOperation, Auth } from "aws-amplify";
@@ -71,36 +71,55 @@ const InputBox = (props) => {
 	};
 
 	return (
-		<View style={styles.container}>
-			<View style={styles.mainContainer}>
-				<FontAwesome5
-					name='laugh-beam'
-					size={24}
-					color='grey'
-					position='fixed'
-				/>
-				<TextInput
-					placeholder='Type a message'
-					style={styles.textInput}
-					multiline
-					value={message}
-					onChangeText={setMessage}
-				/>
-				<Entypo name='attachment' size={24} color='grey' style={styles.icon} />
-				{!message && (
-					<Fontisto name='camera' size={24} color='grey' style={styles.icon} />
-				)}
-			</View>
-			<TouchableOpacity onPress={onPress}>
-				<View style={styles.buttonContainer}>
-					{!message ? (
-						<MaterialCommunityIcons name='microphone' size={28} color='white' />
-					) : (
-						<MaterialIcons name='send' size={28} color='white' />
+		<KeyboardAvoidingView
+			behavior={Platform.OS == "ios" ? "padding" : "height"}
+			keyboardVerticalOffset={90}
+			style={{ width: "100%" }}>
+			<View style={styles.container}>
+				<View style={styles.mainContainer}>
+					<FontAwesome5
+						name='laugh-beam'
+						size={24}
+						color='grey'
+						position='fixed'
+					/>
+					<TextInput
+						placeholder='Type a message'
+						style={styles.textInput}
+						multiline
+						value={message}
+						onChangeText={setMessage}
+					/>
+					<Entypo
+						name='attachment'
+						size={24}
+						color='grey'
+						style={styles.icon}
+					/>
+					{!message && (
+						<Fontisto
+							name='camera'
+							size={24}
+							color='grey'
+							style={styles.icon}
+						/>
 					)}
 				</View>
-			</TouchableOpacity>
-		</View>
+				<TouchableOpacity onPress={onPress}>
+					<View style={styles.buttonContainer}>
+						{!message ? (
+							<MaterialCommunityIcons
+								name='microphone'
+								size={28}
+								color='white'
+							/>
+						) : (
+							<MaterialIcons name='send' size={28} color='white' />
+						)}
+					</View>
+				</TouchableOpacity>
+			</View>
+		</KeyboardAvoidingView>
 	);
 };
 
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, ImageBackground } from "react-native";
+import { View, ImageBackground} from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { FlatList } from "react-native-gesture-handler";
 import ChatMessage from "../components/ChatMessage";
@@ -10,66 +10,62 @@ import { messagesByChatRoom } from "../src/graphql/queries";
 import { onCreateMessage } from "../src/graphql/subscriptions";
 
 const ChatRoomScreen = () => {
-
 	const [messages, setMessages] = useState([]);
 	const [myId, setMyId] = useState(null);
-  
-	const route = useRoute();
-  
-	const fetchMessages = async () => {
-	  const messagesData = await API.graphql(
-		graphqlOperation(
-		  messagesByChatRoom, {
-			chatRoomID: route.params.id,
-			sortDirection: "DESC",
-		  }
-		)
-	  )
-  
-	  console.log("FETCH MESSAGES")
-	  setMessages(messagesData.data.messagesByChatRoom.items);
-	}
-  
-	useEffect(() => {
-	  fetchMessages();
-	}, [])
-  
-	useEffect(() => {
-	  const getMyId = async () => {
-		const userInfo = await Auth.currentAuthenticatedUser();
-		setMyId(userInfo.attributes.sub);
-	  }
-	  getMyId();
-	}, [])
-  
-	useEffect(() => {
-	  const subscription = API.graphql(
-		graphqlOperation(onCreateMessage)
-	  ).subscribe({
-		next: (data) => {
-		  const newMessage = data.value.data.onCreateMessage;
-  
-		  if (newMessage.chatRoomID !== route.params.id) {
-			console.log("Message is in another room!")
-			return;
-		  }
-  
-		  fetchMessages();
-		}
-	  });
-  
-	  return () => subscription.unsubscribe();
-	}, [])
-	return (
-		<ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
-			<FlatList
-				data={messages}
-				renderItem={({ item }) => <ChatMessage myId={myId} message={item} />}
-				inverted
-			/>
 
-			<InputBox chatRoomID={route.params.id} />
-		</ImageBackground>
+	const route = useRoute();
+
+	const fetchMessages = async () => {
+		const messagesData = await API.graphql(
+			graphqlOperation(messagesByChatRoom, {
+				chatRoomID: route.params.id,
+				sortDirection: "DESC",
+			})
+		);
+
+		console.log("FETCH MESSAGES");
+		setMessages(messagesData.data.messagesByChatRoom.items);
+	};
+
+	useEffect(() => {
+		fetchMessages();
+	}, []);
+
+	useEffect(() => {
+		const getMyId = async () => {
+			const userInfo = await Auth.currentAuthenticatedUser();
+			setMyId(userInfo.attributes.sub);
+		};
+		getMyId();
+	}, []);
+
+	useEffect(() => {
+		const subscription = API.graphql(
+			graphqlOperation(onCreateMessage)
+		).subscribe({
+			next: (data) => {
+				const newMessage = data.value.data.onCreateMessage;
+
+				if (newMessage.chatRoomID !== route.params.id) {
+					console.log("Message is in another room!");
+					return;
+				}
+
+				fetchMessages();
+			},
+		});
+
+		return () => subscription.unsubscribe();
+	}, []);
+	return (
+			<ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
+				<FlatList
+					data={messages}
+					renderItem={({ item }) => <ChatMessage myId={myId} message={item} />}
+					inverted
+				/>
+				<InputBox chatRoomID={route.params.id} />
+			</ImageBackground>
 	);
 };
 
